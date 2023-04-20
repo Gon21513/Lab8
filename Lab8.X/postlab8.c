156630c4d3bc06b49d4cb327cf5eb8415392cf03
Gon2153 <125921214+Gon21513@users.noreply.github.com>
2023-04-19 18:29:13 -0600

    PRUEBA 2 POSTLAB
    se implementa conversion  de valor del potenciometro y menu de opciones

--- Lab8.X/postlab8.c
+++ Lab8.X/postlab8.c
@@ -4,11 +4,19 @@
  *
  * Created on 19 de abril de 2023, 03:34 PM
  */
-
-
 #define _XTAL_FREQ 1000000
 
 //variables
+int ascii[10] = {0, 1, 2, 3, 4, 5, 7, 8, 9}; 
+//unsigned int word; //variable para guardar caracter ascii
+unsigned char bandera; //bandera para manetener loop
+unsigned char opcion; //selector para entrar en algun modo
+unsigned char bandera; //bandera para mantener en loop
+unsigned int centena; //variable para centenas en conversión ascii
+unsigned int decena; //variable para decenas en conversión ascii
+unsigned int unidad; //variable para unidades en conversión ascii
+unsigned int pot; //variable para guardar valor del potenciómetro
+
 // CONFIG1
 #pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
@@ -27,164 +35,149 @@
 
 #include <xc.h>
 #include <stdint.h>
-#include <pic16f887.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//----------variables-----------
-uint8_t bandera;
-char selection;
-char RX;
-uint8_t pot;
-char pot_char[];
+#include <pic16f887.h>
 
 
-
-//-----------------prototipos----------
-void setup(void);
-void desplegar(char selection);
-void Print (unsigned char *word);
-//void cadena(char *cursor);
-
-//--------------INTERRUPCION-----------
+// --------------- Rutina de  interrupciones --------------- 
 void __interrupt() isr(void){
-    //recepcion de datos
-    if(PIR1bits.RCIF)//bandera de interrucpcion de eusart
-    {
-        RX = RCREG;//revisamos rcreg y lo guardamos
-    }
-    
-    if (PIR1bits.ADIF){//revisar la interrupcion del adc
-       if (ADCON0bits.CHS == 0b0000){//revisar el canal 0
-           pot = ADRESH;
-       }
-       PIR1bits.ADIF = 0;// reiniciar la interrucipcn del adc
-    }
+    // ---- INTERRUPCION DEL ADC --------
+    if (PIR1bits.ADIF == 1){ // Chequear bandera del conversor ADC
+        pot = ADRESH; // Pasar valor del ADRESH a variable pot
+        centena = (pot/100); //operación para obtener centenas
+        decena = ((pot/10)%10); //operación para obtener decenas
+        unidad = (pot%10); //operación para obtener unidades
+        PIR1bits.ADIF = 0; // Limpiar bandera de conversor
+        }
 }
 
+// --------------- Prototipos --------------- 
+void setup(void);
+void cadena(char *cursor);
 
+// --------------- main --------------- 
 void main(void){
     setup();
-
-    while (1){
-        
-        if (ADCON0bits.GO == 0){
-            ADCON0bits.GO = 1; 
-        }
     
-        
-        Print("\r --------------BIENVENIDO----------------\r");
-        Print("\r ESCOJA UNA DE LAS OPCIONES: \r");
-        Print("\r 1.LEER POTENCIOETRO \r");
-        Print("\r 2.ENVIAR ASCII \r");
+    
+    
+    // Envio de datos
+    while (1){
+        //cadena("\n\rSELECCIONE UNA OPCION\n\r) Leer Potenciometro\n\r2) Enviar ASCII\n\r"); // mostrar el menu como string
+        cadena("SELECCIONE UNA OPCION:\n\r"); //enter
+        cadena("1. VALOR DEL POTENCIOMETRO\n\r"); //enter
+        cadena("2. VALOR EN ASCII\n\r"); //enter
 
         bandera = 1;
+        while (bandera == 1){
+                if (PIR1bits.RCIF == 1){ //revisar bandera de si el recibidor esta lleno 
+                opcion = RCREG; //pasar a dato a varible
+                PIR1bits.RCIF = 0; //limpiar bandera
+                __delay_ms(10);} //delay de 10ms
+                
+            // --------------- Leer POT ---------------    
+            if (opcion == '1'){ //revisar si se selecciono 1
+                cadena("\n\r"); //enter
+                cadena("EL VALOR DEL POTENCIOMETRO ES\n\r"); //enter
+                ADCON0bits.GO = 1; //iniciar conversor ADC
+                __delay_ms(5); //delay de 5ms
+                TXREG = ascii[centena]+48; //mostrar valor de centena
+                __delay_ms(5); //delay de 5ms
+                TXREG = ascii[decena]+48; //mostrar valor de decena
+                __delay_ms(5); //delay de 5ms
+                TXREG = ascii[unidad]+48; //mostrar valor de unidades
+                cadena("\n\r\n\r"); //enter
+                bandera = 0; //regresar al menu
+                opcion = 0; //limpiar selector
+            }
+            
+            // --------------- Enviar ASCII --------------- 
+            if (opcion == '2'){ //revisar si se selecciono 2
+                cadena("\n\r"); //enter
+                cadena("INGRESE UN CARACTER PARA MOSTRAR EN ASCII\n\r"); //mostrar informacion
+                cadena("\n\r"); //enter
+                bandera = 1; //encender bandera para esperar que se introduzca tecla
+                PIR1bits.RCIF = 0; //limpiar bandera 
+                while (bandera == 1){ //loop
+                if (PIR1bits.RCIF == 1 && RCREG != 0){ //esperar que se presione tecla  
+                PORTB = RCREG; //mostrar valor en el puerto B
+                PIR1bits.RCIF = 0; //limpiar bandera
+                bandera = 0; //apagar bandera
+                opcion = 0;}}} //apagar bandera
         
-        while (bandera){//chequear la bandera
-            
-            while (PIR1bits.RCIF == 0); //recibimos datos
-            selection = RX;
-            
-            switch(selection){
-                
-                case ('1'):
-                    Print("\r VALOR DEL POTENCIOMETRO: \r");
-                    itoa(pot_char, pot,10);
-                    desplegar(pot_char);
-                    bandera = 0;
-                    break;
-            
-            
-            case ('2'):
-                Print("\r INGRESE UN CARACTER PARA MOSTRAR EN ASCII: \r");
-                
-                while (PIR1bits.RCIF == 0);
-                    PORTB = RX; 
-                    desplegar(RX);
-                    bandera = 0;
-                    break;
-                }
-            
-        }
-    
+            }
+        }  
+        __delay_ms(500);                
     }
-    return;
-}
 
 
-
-//-----------set up general--------
+// --------------- Setup General --------------- 
 void setup(void){
-    ANSEL = 0b00000001; //ans0 como entrda analogivs
+    // --------------- Definir analogicas --------------- 
+    ANSEL = 0b00000001; // Habilitar AN0 como analogica
     ANSELH = 0;
     
-    TRISB = 0;//configurar portd como salids
-    PORTB = 0;//iniciar el puerto d
+    TRISB = 0; // PORTB como salida
+    PORTB = 0; // Iniciar PORTB
     
-//--------------oscilador----------------
+// --------------- Oscilador --------------- 
+    OSCCONbits.IRCF = 0b100; // 8 MHz
+    OSCCONbits.SCS = 1; // Seleccionar oscilador interno
 
+    // --------------- Banderas e interrupciones ---------------
+ 
+    PIE1bits.ADIE = 1; // Habiliar interrupcion del conversor ADC
+    PIR1bits.ADIF = 0; // Limpiar bandera de interrupción del ADC
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
     
-
-    OSCCONbits.IRCF = 0b100; //1mhz
-    OSCCONbits.SCS = 1;//oscilador interno
-
-    
-//-------configuracion de tx y rx-----------
-    TXSTAbits.SYNC = 0;//asincrono
+//---------------SETUP TX Y RX------------------
+        TXSTAbits.SYNC = 0;//asincrono
     TXSTAbits.BRGH = 1;//high baud rate select bit
     
     BAUDCTLbits.BRG16 = 1;//utilizar q6 bits baud rate
     
-
     SPBRG = 25; //configurar a 9615
-    SPBRGH = 0;
-   
+    SPBRGH = 0;    
+
+    
     RCSTAbits.SPEN = 1;//habilitar la comunicacion serial
-    TXSTAbits.TX9 = 0; //USAR SOLO 8 BITS    
+    RCSTAbits.RX9 = 0;//deshabiliamos bit de direccion
     RCSTAbits.CREN = 1;//habilitar recepcion 
     TXSTAbits.TXEN = 1;//habiliar la transmision
     
     
-//------------configuracion de interrupciones-------
-    PIE1bits.RCIE = 1;//interrupcion de la recepcion 
-    INTCONbits.PEIE = 1;
-    PIE1bits.ADIE = 1; //interrupcion del adc
-    PIR1bits.ADIF = 0; //bandera del adc en 0
-    INTCONbits.GIE = 1;
+//--------------------ADC-------------------------
+        // --------------- Configura el canal --------------- 
+    ADCON0bits.CHS = 0b0000; // seleccionar AN0
     
-    
-//------------configuracion del adc-----------    
-    ADCON0bits.ADCS = 0b01; ; //forc8
-    
+            
     // --------------- Seleccion voltaje referencia --------------- 
     ADCON1bits.VCFG1 = 0; // Voltaje de referencia de 0V
     ADCON1bits.VCFG0 = 0; // Voltaje de referencia de 5V
+            
+    // --------------- Seleccion de reloj ---------------
+    ADCON0bits.ADCS = 0b10; // Fosc/32
+            
+    // --------------- Habilitar interrupciones del ADC ---------------
     
             
-    // --------------- justificado izquierda ---------------
-    ADCON1bits.ADFM = 0; 
-    
-    // --------------- canales --------------- 
-    ADCON0bits.CHS = 0b0000; // seleccionar AN0
-           
+    // --------------- Asignar 8 bits, justificado izquierda ---------------
+    ADCON1bits.ADFM = 0;        
             
     //--------------- Iniciar el ADC ---------------
-    ADCON0bits.ADON = 1;  //INICIA EL ADC
-    __delay_us(50);
+    ADCON0bits.ADON = 1;  
+    __delay_ms(1);
 }
+    
 
-//funcion para enviar caracteres
-void Print(unsigned char *word){
-    while(*word != '\0'){
-        while (TXIF != 1);
-        TXREG = *word;
-        *word++;
+
+//Funcion para mostrar texto
+void cadena(char *cursor){
+    while (*cursor != '\0'){ // Mientras el cursor sea diferente a nulo
+        while (PIR1bits.TXIF == 0); // Mientras que se este enviando no hacer nada
+            TXREG = *cursor; // Asignar el valor del cursor para enviar
+            *cursor++; // Aumentar posicion del cursor
     }
-    return;
 }
 
-void desplegar (char selection){
-    while (TXSTAbits.TRMT == 0);
-    TXREG = selection;
-}
